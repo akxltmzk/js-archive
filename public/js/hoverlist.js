d40d8da2dbb9a4eeb9a2ef93241ab563832130be
hyunwoo-do <akxltmzk@gmail.com>
2021-06-13 18:29:23 +0900

    hover image

--- public/js/hoverlist.js
+++ public/js/hoverlist.js
@@ -1,65 +1,78 @@
+let project_preview = document.querySelector('.project-preview')
+let navigation_item_all = document.querySelectorAll('.navigation-item')
+let navigation_link_1 = document.querySelector('.navigation-link-1')
+let navigation_link_2 = document.querySelector('.navigation-link-2')
+let navigation_link_3 = document.querySelector('.navigation-link-3')
+let navigation_link_4 = document.querySelector('.navigation-link-4')
+let navigation_link_5 = document.querySelector('.navigation-link-5')
+let navigation_link_6 = document.querySelector('.navigation-link-6')
+let navigation_link_7 = document.querySelector('.navigation-link-7')
+let navigation_link_8 = document.querySelector('.navigation-link-8')
+
 document.addEventListener('DOMContentLoaded', function () {
-  
-}, false);
 
-// $(document).ready(function() {
-//   TweenMax.set(".project-preview", { width: 0 });
+  let tl = new TimelineLite()
 
-//   var tl = new TimelineLite();
+  TweenMax.set(".project-preview", { width: 0 })
 
-//   $(document)
-//     .on("mouseover", ".navigation-item", function(evt) {
-//       tl = new TimelineLite();
-//       tl.to($(".project-preview"), 1, {
-//         width: "600px",
-//         ease: Expo.easeInOut
-//       });
-//     })
-//     .on("mouseout", ".navigation-item", function(evt) {
-//       tl = new TimelineLite();
-//       tl.to($(".project-preview"), 0.5, {
-//         width: 0,
-//         ease: Expo.easeInOut
-//       });
-//     });
-// });
+  navigation_item_all.forEach(element => {
 
-// $(".navigation-link-1").hover(function() {
-//   $(".project-preview").css({ "background-image": "url(img-1.jpg)" });
-// });
+    element.addEventListener('mouseover',function(e){
+      tl = new TimelineLite()
+      tl.to(project_preview, 1, {
+        width: "600px",
+        ease: Expo.easeInOut
+      })
+    })
 
-// $(".navigation-link-2").hover(function() {
-//   $(".project-preview").css({ "background-image": "url(img-2.jpg)" });
-// });
+    element.addEventListener('mouseout',function(e){
+      tl = new TimelineLite();
+      tl.to(project_preview, 0.5, {
+        width: 0,
+        ease: Expo.easeInOut
+      })
+    })
+  }, false)
 
-// $(".navigation-link-3").hover(function() {
-//   $(".project-preview").css({ "background-image": "url(img-3.jpg)" });
-// });
+}, false)
 
-// $(".navigation-link-4").hover(function() {
-//   $(".project-preview").css({ "background-image": "url(img-4.jpg)" });
-// });
+navigation_link_1.addEventListener('mouseover',function(){
+  project_preview.style.backgroundImage = 'url(../images/hoverlist/img-1.jpg)'
+})
 
-// $(".navigation-link-5").hover(function() {
-//   $(".project-preview").css({ "background-image": "url(img-5.jpg)" });
-// });
+navigation_link_2.addEventListener('mouseover',function(){
+  project_preview.style.backgroundImage = 'url(../images/hoverlist/img-2.jpg)'
+})
 
-// $(".navigation-link-6").hover(function() {
-//   $(".project-preview").css({ "background-image": "url(img-6.jpg)" });
-// });
+navigation_link_3.addEventListener('mouseover',function(){
+  project_preview.style.backgroundImage = 'url(../images/hoverlist/img-3.jpg)'
+})
 
-// $(".navigation-link-7").hover(function() {
-//   $(".project-preview").css({ "background-image": "url(img-7.jpg)" });
-// });
+navigation_link_4.addEventListener('mouseover',function(){
+  project_preview.style.backgroundImage = 'url(../images/hoverlist/img-4.jpg)'
+})
 
-// $(".navigation-link-8").hover(function() {
-//   $(".project-preview").css({ "background-image": "url(img-8.jpg)" });
-// });
+navigation_link_5.addEventListener('mouseover',function(){
+  project_preview.style.backgroundImage = 'url(../images/hoverlist/img-5.jpg)'
+})
 
-// $(window).scroll(function() {
-//   var scroll = $(window).scrollTop(),
-//     dh = $(document).height(),
-//     wh = $(window).height();
+navigation_link_6.addEventListener('mouseover',function(){
+  project_preview.style.backgroundImage = 'url(../images/hoverlist/img-6.jpg)'
+})
+
+navigation_link_7.addEventListener('mouseover',function(){
+  project_preview.style.backgroundImage = 'url(../images/hoverlist/img-7.jpg)'
+})
+
+navigation_link_8.addEventListener('mouseover',function(){
+  project_preview.style.backgroundImage = 'url(../images/hoverlist/img-8.jpg)'
+})
+
+
+// window.addEventListener('scroll',function(){
+//   let scroll = $(window).scrollTop(),
+//     dh = document.height(),
+//     wh = window.height();
 //   scrollPercent = (scroll / (dh - wh)) * 100;
 //   $(".progressbar").css("height", scrollPercent + "%");
-// });
+// })
